f277b00c3eb642a8a6e9494f31b0e6e50beaf8ee
kakigakki <kakigakki@yahoo.co.jp>
2021-02-01 14:40:25 +0900

    vue3todo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center items-center bg-purple-300 h-screen w-screen">
     <div class="w-c1 h-auto bg-gray-50 rounded-3xl shadow-2xl">
-      <div class="mx-8 pb-10">
+      <div class="mx-8 pb-10 relative">
         <h1 class="font-bold text-5xl text-purple-900 mt-24">Hi,KAKI</h1>
         <!-- 搜索框 -->
         <SearchBox @addToList="addToList"></SearchBox>
@@ -18,9 +18,8 @@ import SearchBox from "./components/SearchBox.vue";
 import List from "./components/List.vue";
 
 interface IntertodoList {
-  all: string[];
-  done: string[];
-  noDone: string[];
+  value: string;
+  isDone: boolean;
 }
 export default defineComponent({
   name: "App",
@@ -30,14 +29,9 @@ export default defineComponent({
   },
   emits: ["addToList"],
   setup() {
-    const todoList = reactive<IntertodoList>({
-      all: [],
-      done: [],
-      noDone: [],
-    });
+    const todoList = reactive<IntertodoList[]>([]);
     const addToList = (data: Ref) => {
-      todoList.all.unshift(data.value);
-      todoList.noDone.unshift(data.value);
+      todoList.unshift({ value: data.value, isDone: false });
     };
     return {
       todoList,
--- src/components/List.vue
+++ src/components/List.vue
@@ -10,38 +10,51 @@
     >
   </div>
   <!-- 列表 -->
-  <transition name="slide-fade" v-for="(item, index) in lists" :key="index">
-    <div v-show="tabIndex === index">
-      <transition-group name="list-complete">
-        <label
-          v-for="(item2, indey) in lists[index]"
-          :key="item2"
-          :for="indey"
-          class="flex items-center text-c1 w-full px-4 py-5 bg-white my-5 rounded-lg shadow-sm cursor-pointer transition-transform duration-500"
-        >
-          <input
-            type="checkbox"
-            :id="indey"
-            class="form-tick appearance-none h-7 w-7 border-purple-400 border-3 border-solid rounded-full checked:bg-purple-400 focus:outline-none mr-3"
-          />
-          <span class="checked-sibling:line-through">{{ item2 }}</span>
-        </label>
-      </transition-group>
-    </div>
-  </transition>
+  <transition-group name="list-complete">
+    <label
+      v-for="(item, index) in filterList(tabIndex)"
+      :key="item"
+      :for="index"
+      class="flex items-center text-c1 w-full px-4 py-5 bg-white my-5 rounded-lg shadow-sm cursor-pointer transition duration-300"
+      @click.prevent="item.isDone = !item.isDone"
+    >
+      <input
+        type="checkbox"
+        :id="index"
+        class="form-tick appearance-none h-7 w-7 border-purple-400 border-3 border-solid rounded-full checked:bg-purple-400 focus:outline-none mr-3"
+        :checked="item.isDone"
+      />
+      <span class="checked-sibling:line-through">{{ item.value }}</span>
+    </label>
+  </transition-group>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+interface IntertodoList {
+  value: string;
+  isDone: boolean;
+}
 export default defineComponent({
   name: "List",
   props: ["todoList"],
   setup(props) {
     const tabIndex = ref(0);
     const tabName = ["全部", "已完成", "未完成"];
-    const lists = Object.values(props.todoList);
+    const filterList = (index: number) => {
+      if (index > 0) {
+        return props.todoList.filter(
+          (item: IntertodoList) => item.isDone === (index === 1 ? true : false)
+        );
+      }
+      return props.todoList;
+    };
+    const add = () => {
+      console.log(1);
+    };
     return {
-      lists,
+      add,
+      filterList,
       tabName,
       tabIndex,
     };
@@ -52,21 +65,6 @@ export default defineComponent({
 <style scoped>
 .active {
   @apply transform scale-110 text-purple-300;
-}
-
-.slide-fade-enter-active,
-.slide-fade-leave-active {
-  transition: all 0.3s ease-out;
-}
-
-.slide-fade-enter-active {
-  transition-delay: 0.4s;
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateX(20px);
-  opacity: 0;
 }
 
 .list-complete-enter-from,
